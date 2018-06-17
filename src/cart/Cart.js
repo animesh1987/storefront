6bce8f7bc6beeccad657ed99c60ffe624643b1e9
Animesh <animesh@investmytalent.com>
2018-06-17 20:54:31 +1000

    - add cart footer + buttons

--- src/cart/Cart.js
+++ src/cart/Cart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { removeFromCart } from '../store/actions/cart';
 import { CartProduct } from './cart-product/Cart-Product';
 import './Cart.css';
@@ -9,29 +10,48 @@ class Cart extends Component {
     return (
       <div className="Cart">
           <h2>Shopping Cart</h2>
-          <table className="Cart__container">
-            <thead>
-              <tr className="Cart__container-header">
-                <th>Product</th>
-                <th>Quantity</th>
-                <th>Total</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.props.cart.map(product =>
-                <CartProduct key={product.id}
-                  product={product} />)}
-            </tbody>
-          </table>
+          <div className="Cart__container">
+            <table>
+              <thead>
+                <tr className="Cart__container-header">
+                  <th>Product</th>
+                  <th>Quantity</th>
+                  <th>Total</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody className="flex flex-column">
+                {this.props.cart.map(product =>
+                  <CartProduct key={product.id}
+                    product={product} />)}
+              </tbody>
+            </table>
+            <div className="Cart__overview-container">
+              <div className="Cart__overview flex flex-column">
+                <p>Cart Overview</p>
+                <p className="flex flex-row">
+                  Subtotal <span>${this.props.total}.00</span></p>
+                <p className="flex flex-row">
+                  Total <span>${this.props.total}.00 AUD</span></p>
+              </div>
+            </div>
+            <div className="flex flex-row Cart_actions">
+              <Link to={'/'}>Continue Shopping</Link>
+              <button>Checkout(${this.props.total}.00)</button>
+            </div>
+          </div>
       </div>
     );
   }
 }
 
 const mapStateToPros = state => {
+  const cartTotal = state.cart.cart.length > 0 ?
+    state.cart.cart.map(product => product.quantity * product.price)
+    .reduce((a, b) => a + b) : 0
   return ({
     cart: state.cart.cart,
+    total: cartTotal,
   })
 };
 
